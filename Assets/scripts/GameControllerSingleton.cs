ddd30effd2d3f22911d571ae23cb15042422f3a5
Pedro Alba <pedrohalba@hotmail.com>
2019-05-22 21:25:31 -0300

    .

--- Assets/scripts/GameControllerSingleton.cs
+++ Assets/scripts/GameControllerSingleton.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 using System.Linq;
 public class GameControllerSingleton : Singleton<GameControllerSingleton>
 {
-    public int currentLevel = 1;
+    public int currentLevel = 0;
     public string MyTestString = "Hello world!";
 
+    public GameObject instance;
     public void nextLevel(){ 
          currentLevel++;
          var keyValuePair = Palavras.niveis.Where(p => p.Value == currentLevel).First();
          Debug.Log(keyValuePair.Value);
 
-        GameObject instance = Instantiate(Resources.Load(keyValuePair.Key.ToString(), typeof(GameObject))) as GameObject;
+        Destroy(instance);
+        instance = Instantiate(Resources.Load(keyValuePair.Key.ToString(), typeof(GameObject))) as GameObject;
         instance.transform.position = new Vector3(10, 5, 15);
         instance.transform.rotation = new Quaternion(0, 0, 0, 0);
         instance.transform.localScale = new Vector3(20, 20, 20);
