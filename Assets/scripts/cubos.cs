23f9e6a77983c7adab1db9e1b82937f7c1cbda71
Matheus Waltrich <matheuswaltrich@gmail.com>
2019-05-29 19:38:03 -0300

    Verifica palavra

--- Assets/scripts/cubos.cs
+++ Assets/scripts/cubos.cs
@@ -6,19 +6,20 @@ using UnityEngine;
 
 public class cubos : MonoBehaviour
 {                
+
+    public int contpalavra = 0;
     string palavra;
     // Start is called before the first frame update
     void Start()
     {        
-        palavra = "CA";
-        //int t = palavra->Length;
+        palavra = "CA";         
         Debug.Log(palavra.Length);
 
         for(int i = 0; i < palavra.Length ; i++){
             GameObject cubo = GameObject.CreatePrimitive(PrimitiveType.Plane); 
             cubo.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
             cubo.transform.rotation = Quaternion.Euler(-10, 0, 0);
-            cubo.transform.position = new Vector3((i*7)-14, 0, 30f);
+            cubo.transform.position = new Vector3((i*7)-10, 0, 30f);
             cubo.name = i.ToString();
         }        
     }
@@ -27,5 +28,13 @@ public class cubos : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void PalavraSoma(){
+        contpalavra++;
+    }
+
+    public void PalavraSubtrai(){
+        contpalavra--;
     }
 }
