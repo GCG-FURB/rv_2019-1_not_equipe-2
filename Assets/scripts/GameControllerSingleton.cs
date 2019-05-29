945a3a2084d7968906d4a17af3f497b3d0650844
Pedro Alba <pedrohalba@hotmail.com>
2019-05-29 19:39:43 -0300

    get current word

--- Assets/scripts/GameControllerSingleton.cs
+++ Assets/scripts/GameControllerSingleton.cs
@@ -1,15 +1,16 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
 public class GameControllerSingleton : Singleton<GameControllerSingleton>
 {
-    public int currentLevel = 0;
+    public int currentLevel = 1;
     public string MyTestString = "Hello world!";
 
     public GameObject instance;
     public void nextLevel(){ 
-         currentLevel++;
+         
          var keyValuePair = Palavras.niveis.Where(p => p.Value == currentLevel).First();
          Debug.Log(keyValuePair.Value);
 
@@ -18,5 +19,11 @@ public class GameControllerSingleton : Singleton<GameControllerSingleton>
         instance.transform.position = new Vector3(10, 5, 15);
         instance.transform.rotation = new Quaternion(0, 0, 0, 0);
         instance.transform.localScale = new Vector3(20, 20, 20);
+        currentLevel++;
+    }
+
+    public String getCurrentWord(){
+         var keyValuePair = Palavras.niveis.Where(p => p.Value == currentLevel).First();
+         return keyValuePair.Key.ToString();
     }
 }
